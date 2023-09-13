99994c5587dffabdda1c386d88184cf30a51bfd9
kobunsei <kou@justsolution.co.jp>
2023-09-13 10:57:09 +0900

    ver5

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ logging.basicConfig(level=logging.INFO,
 format="%(asctime)s - %(levelname)s:%(name)s - %(message)s",
 filename="test.log")
 logging.info('hello python test515 log')
-filename='test.log'
+
 print('hello python test515')
